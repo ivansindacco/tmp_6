c905fa9b182aff6e4c2d0b9f3b791be8399d4e44
ivansindacco <56919658+ivansindacco@users.noreply.github.com>
2024-06-21 12:25:53 +0300

    Add files via upload

---
+++ pr6_tmp_3.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+
+class GraphicComponent {
+public:
+	virtual void draw() = 0;
+};
+
+class SimpleGraphic : public GraphicComponent {
+public:
+	void draw() override {
+		std::cout << "Drawing Simple Graphic" << std::endl;
+	}
+};
+
+class CompositeGraphic : public GraphicComponent {
+private:
+	std::vector<GraphicComponent*> components;
+
+public:
+	void addComponent(GraphicComponent* component) {
+		components.push_back(component);
+	}
+
+	void draw() override {
+		std::cout << "Drawing Composite Graphic" << std::endl;
+
+		for (auto component : components) {
+			component->draw();
+		}
+	}
+};
+
+int main() {
+	SimpleGraphic simpleGraphic1;
+	SimpleGraphic simpleGraphic2;
+	SimpleGraphic simpleGraphic3;
+
+	CompositeGraphic compositeGraphic;
+	compositeGraphic.addComponent(&simpleGraphic1);
+
+	CompositeGraphic nestedComposite;
+	nestedComposite.addComponent(&simpleGraphic2);
+	nestedComposite.addComponent(&simpleGraphic3);
+
+	compositeGraphic.addComponent(&nestedComposite);
+
+	compositeGraphic.draw();
+
+	return 0;
+}
