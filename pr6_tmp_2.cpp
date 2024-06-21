d71e902020a45ff0028812ee8ad510b50cfe36e2
ivansindacco <56919658+ivansindacco@users.noreply.github.com>
2024-06-21 11:59:29 +0300

    Add files via upload

---
+++ pr6_tmp_2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+class IService {
+public:
+    virtual void request() = 0;
+};
+
+class RealService : public IService {
+public:
+    void request() override {
+        std::cout << "RealService: Obrabotka ." << std::endl;
+    }
+};
+
+class ProxyService : public IService {
+private:
+    RealService* realService;
+
+public:
+    ProxyService() : realService(nullptr) {}
+
+    void request() override {
+        if (realService == nullptr) {
+            realService = new RealService();
+        }
+        std::cout << "ProxyService: Peredacha." << std::endl;
+        realService->request();
+    }
+
+    ~ProxyService() {
+        delete realService;
+    }
+};
+
+int main() {
+    ProxyService proxy;
+
+    proxy.request();
+
+    proxy.request();
+
+    return 0;
+}
